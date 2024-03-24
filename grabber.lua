712efdd8b9e84195429b2c45e5af45c881ffd32c
iMakeMehThings <100486180+iMakeMehPrograms@users.noreply.github.com>
2024-03-24 16:23:11 -0500

    Update grabber.lua

--- grabber.lua
+++ grabber.lua
@@ -4,7 +4,7 @@ if table.getn(args) < 2 then
   error("Two arguments must be given: a vaild HTTP URL where the raw file is hosted, and an endpoint filename.", 1)
 else
   print("Arguments are: " .. args[1] .. " and " .. args[2])
-  if http.checkURL() == false then
+  if http.checkURL(args[1]) == false then
     error("URL cannot be requested.", 1)
   end
   local http_grabber = http.get(args[1])
