bd6bab124b873922f4fb0c0c0c4b57e45417f21f
iMakeMehThings <100486180+iMakeMehPrograms@users.noreply.github.com>
2024-03-24 15:37:35 -0500

    Added the file grabber, which would be handy to install the file grabber on the computer.

---
+++ grabber.lua
@@ -0,0 +1,29 @@
+local args = {...}
+
+if table.getn(args) < 2 then
+  error("Two arguments must be given: a vaild HTTP URL where the raw file is hosted, and an ending filename.", 1)
+else
+  local http_grabber = http.get(args[1]).readAll()
+  if h then
+    if not fs.exists(args[2]) then
+      f = fs.open(args[2], "w")
+      f.write(http_grabber)
+      f.close()
+      print("File " .. args[2] .. " was downloaded successfully.")
+    else
+      print("File already exists, should this update and replace it? [y/n]")
+      local event, char = os.pullEvent("char")
+      if char == "y" then
+        f = fs.open(args[2], "w+")
+        f.write(http_grabber)
+        f.close()
+        print("File overrided.")
+      else 
+        print("Cancelling action.")
+      end
+    end
+  else
+    error("HTTP URL not found.", 1)
+  end
+end
+
